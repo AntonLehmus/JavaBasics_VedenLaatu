731e069c5cf980fe69447e23c930de5c239cebd6
Anton Lemus <anton.lehmus@gmail.com>
2016-02-29 18:16:27 +0200

    main menu

--- src/main/java/com/lehmusa/vedenlaatu/WaterService.java
+++ src/main/java/com/lehmusa/vedenlaatu/WaterService.java
@@ -10,6 +10,7 @@ import com.google.gson.GsonBuilder;
 import static java.lang.System.out;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 
@@ -23,10 +24,11 @@ public class WaterService {
     
     
     public static void main(String[] args) {
-        WaterService waterService = new WaterService();
+        WaterService main = new WaterService(); 
+        main.mainMenu();
      
     }
-    public WaterService() {
+    public void getWaterData() {
         // Luo HTTPThread -säie ja käynnistä se
         HttpThread httpThread = new HttpThread( 
                 "https://vellamo.tampere.fi/api/v1/latest.json", 
@@ -37,19 +39,81 @@ public class WaterService {
 
                         waterArray = new GsonBuilder().create().
                                 fromJson(jsonResponse, Vedenlaatu[].class);
-                        
-                        ArrayList<Integer> temp = findArea("hervanta");
-                        if(temp.size()>0){
-                            for (Integer num : temp) {
-                                out.println(waterArray[num].getSlug().toString());
-                                 printLatestMeasurementes(waterArray[num].getLatestMeasurements());
-                            }
-                        }	      
-                     
                     }
                 });
         httpThread.start();
     }
+    private void mainMenu(){
+        String strInput;
+        int input = 0;
+        
+        showOptions();
+        
+        strInput = JOptionPane.showInputDialog("Toiminto: ");
+        if(strInput != null){
+         input = Integer.parseInt( strInput);
+        }
+        else{
+            out.println("");
+            out.println("Valitse jokin vaihtoehto!");
+            out.println("");
+            out.println("");
+            mainMenu();
+        }
+        
+        
+        switch (input){
+            case 1:
+                getWaterData();
+                 out.println("Data päivitetty!");
+                break;
+            case 2:
+                String area="";
+                showResults(area);
+                break;
+            case 3:
+                listLocations();
+                break;
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                out.println("Virheellinen syöte!");
+                out.println("");
+                out.println("");
+                mainMenu();    
+                break;
+                 
+        }
+    }
+    
+    private void listLocations(){
+        out.println("TODO");
+    }
+    
+    private void showResults(String area){
+            ArrayList<Integer> temp = findArea(area);
+                if(temp.size()>0){
+                    for (Integer num : temp) {
+                            out.println(waterArray[num].getSlug().toString());
+                            printLatestMeasurementes(waterArray[num].getLatestMeasurements());
+                            }
+                    }
+                else{
+                    out.println("Aluetta ei löytynyt!");
+                    out.println("");
+                    mainMenu();
+                }
+    }
+    
+    private void showOptions(){
+        out.println("1.Päivitä data (uutta dataa saatavissa tunnin välein)");
+        out.println("2.Näytä data sijainnin perusteella");
+        out.println("3.Listaa sijainnit");
+        out.println("4.poistu");
+        
+    }
+    
     private void printLatestMeasurementes(LatestMeasurements current){
         DecimalFormat df = new DecimalFormat("##.#");
         
