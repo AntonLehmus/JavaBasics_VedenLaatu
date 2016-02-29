ecb7c4eda5d08fd8f688587b1ba404a539c28884
Anton Lemus <anton.lehmus@gmail.com>
2016-02-29 13:13:18 +0200

    alueitten lisäsys

---
+++ src/main/java/com/lehmusa/vedenlaatu/Area.java
@@ -0,0 +1,54 @@
+
+package com.lehmusa.vedenlaatu;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class Area {
+
+    @SerializedName("slug")
+    @Expose
+    private String slug;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    /**
+     * 
+     * @return
+     *     The slug
+     */
+    public String getSlug() {
+        return slug;
+    }
+
+    /**
+     * 
+     * @param slug
+     *     The slug
+     */
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
