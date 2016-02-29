e197a0db43142eec72f3ff3087d76051ff446706
Anton Lemus <anton.lehmus@gmail.com>
2016-02-29 11:24:33 +0200

    Projektin aloitus

---
+++ src/main/java/com/lehmusa/vedenlaatu/WaterService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lehmusa.vedenlaatu;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author Anton
+ */
+public class WaterService {
+    public static void main(String[] args) {
+        WaterService waterService = new WaterService();
+     
+    }
+    public WaterService() {
+        // Luo HTTPThread -säie ja käynnistä se
+        HttpThread httpThread = new HttpThread( 
+                "https://vellamo.tampere.fi/api/v1/latest.json", 
+                new HttpThreadListener() {
+            @Override
+            public void JsonResponseReady(String jsonResponse) {
+                out.println(jsonResponse);
+                   
+            }
+    });
+        httpThread.start();
+    }
+}
